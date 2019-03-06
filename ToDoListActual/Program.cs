09f199e3e5f9043ef3793017261572769208e1b7
Daniel Hineline <danielhineline3@outlook.com>
2019-03-06 16:29:12 -0500

    Final

--- ToDoListActual/Program.cs
+++ ToDoListActual/Program.cs
@@ -10,16 +10,16 @@ namespace ToDoListActual
     {
         static void Main(string[] args)
         {
-
+            List<string> tasks = new List<string>();
 
             var quit = false;
-            var tasks = new List<string>();
+            //var tasks = new List<string>();
 
-            //Load the l
+            //Load the file HERE.
 
             do
             {
-                //Main user interface
+                //Main user interface.
                 Console.Clear();
                 Console.WriteLine("Task List");
                 Console.WriteLine("--------");
@@ -32,15 +32,14 @@ namespace ToDoListActual
                 Console.WriteLine("  Select your item: ");
                 var input = Console.ReadLine();
 
-                    
                 if (input == "0")
                 {
-                    //Quit the program
+                    //Quit the program.
                     quit = true;
                 }
                 else if (input == "1")
                 {
-                    //Enter new task here
+                    //Enter new task here.
                     Console.WriteLine("Enter your task: ");
                     var newTask = Console.ReadLine();
 
@@ -48,18 +47,23 @@ namespace ToDoListActual
 
                 }
                 else if (input == "2")
+                    
                 {
-
                     Console.Clear();
-                    //Print the list here
+                    //Print the list here.
                     for (int i = 0; i < tasks.Count; i++)
                     {
-                        //The square brackets access one index in the list
-                        //It's important we start at 0 becuase lists and arrays are 0-indexed
+                        //The square brackets access one index in the list.
+                        //It's important we start at 0 becuase lists and arrays are 0-indexed.
                         Console.WriteLine($"{i + 1}. {tasks[i]}");
-                        Console.BackgroundColor = ConsoleColor.Black;
+
                     }
+                    //From the list menu - select the next options.
+                    //Want to be able to select individual item according to # as well as return to the main menu.
+                    Console.WriteLine("Press the corresponding number to select an item from the list.");
                     Console.WriteLine("Press any key to return to the main menu: ");
+                    int a = int.Parse(Console.ReadLine());
+                    tasks[(a) - 1] = changeColor();
                     Console.ReadKey();
                 }
                 else
@@ -69,8 +73,14 @@ namespace ToDoListActual
                 }
 
             } while (!quit);
+
                         
-        }            //Save the list from a file HERE
+        }            //Save the list from a file HERE.
+            static string changeColor()
+            {
+            string b = Console.ForegroundColor = ConsoleColor.DarkGray("");
+                return  Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
         
     }
 
